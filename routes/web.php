e62049e0c13e1db6cd2f866aa541302010c92251
mponsart <maxime.ponsart@speed-cloud.fr>
2026-05-19 22:18:44 +0200

    feat: ajouter la route pour le tableau de bord avec protection par authentification

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
@@ -15,3 +16,7 @@ Route::get('/', function () {
 Route::get('/login', [AuthController::class, 'redirectToGoogle'])->name('login');
 Route::get('/auth/callback', [AuthController::class, 'handleGoogleCallback']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
